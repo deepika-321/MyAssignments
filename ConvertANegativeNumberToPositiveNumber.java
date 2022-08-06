7130705eb088c77a50851313eeb69ade694e0a7c
deepika-321 <110736276+deepika-321@users.noreply.github.com>
2022-08-06 22:04:51 +0530

    Add files via upload
    I have completed day1 assignments.

---
+++ ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		int number=-40;
+		
+		if(number<0)
+		{
+			System.out.println(number*(-1));
+		}
+				
+	}
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+ public class TwoWheeler {
+	
+  public static void main(String[] args) {
+		
+    	    int noOfWheels=2;
+    	    short noOfMirrors=2;
+    		long chassisNumber=876545856;
+    		boolean ispuntured =true;
+    		String bikeName="Discover";
+    	    double runningKM=176.8;
+     
+     System.out.println(noOfWheels);
+     System.out.println(noOfMirrors);
+     System.out.println(chassisNumber);
+     System.out.println(ispuntured);
+     System.out.println(bikeName);
+     System.out.println(runningKM);
+	}
+}
